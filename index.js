f9fd5306290276eb22b870f00cd58f4226d81d3a
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-08-24 20:20:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+// function moving to the right
+dodger = document.getElementById('dodger')
+function moveDodgerLeft() {
+    let left = dodger.style.left
+    left = parseInt(left)
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+    document.addEventListener("keydown", function (event) {
+        if (e.key === "ArrowLeft") {
+          moveDodgerLeft();
+        }
+      });
+  }
+
+
+//   function moving to the left
+
+  function moveDodgerRight() {
+    let left = dodger.style.left
+    left = parseInt(left)
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (event) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
